b1eeb093838a8db5b5d9e4da2f7c47adc0a1ee66
Richard Brown <rabrown@quantumcheese.com>
2012-12-17 17:06:23 -0800

    Final code; displays normal comics correctly; untested for exceptional comics.

--- Xkcd Reader/QCViewController.h
+++ Xkcd Reader/QCViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface QCViewController : UIViewController
+@interface QCViewController : UIViewController <NSXMLParserDelegate>
+{
+    __weak IBOutlet UIImageView *comicView;
+    __weak IBOutlet UITextField *altText;
+    NSString *next, *previous;
+    BOOL insideComicDiv;
+}
+
+- (IBAction)goToFirst:(id)sender;
+- (IBAction)goToPrevious:(id)sender;
+- (IBAction)goToRandom:(id)sender;
+- (IBAction)goToNext:(id)sender;
+- (IBAction)goToLast:(id)sender;
 
 @end
--- Xkcd Reader/QCViewController.m
+++ Xkcd Reader/QCViewController.m
@@ -8,22 +8,103 @@
 
 #import "QCViewController.h"
 
-@interface QCViewController ()
-
-@end
-
 @implementation QCViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self goToLast:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goToFirst:(id)sender
+{
+    [self parsePageAtURL:[NSURL URLWithString:@"http://xkcd.com/1/"]];
+    previous = @"1";
+}
+
+- (IBAction)goToPrevious:(id)sender
+{
+    [self parsePageAtURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://xkcd.com/%@", previous]]];
+}
+
+- (IBAction)goToRandom:(id)sender
+{
+    [self parsePageAtURL:[NSURL URLWithString:@"http://dynamic.xkcd.com/random/comic/"]];
+}
+
+- (IBAction)goToNext:(id)sender
+{
+    [self parsePageAtURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://xkcd.com/%@", next]]];
+}
+
+- (IBAction)goToLast:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:@"http://xkcd.com/"];
+    [self parsePageAtURL:url];
+}
+
+- (void)parsePageAtURL:(NSURL *)url
+{
+    // 1. grab the webpage at `url'
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
+    [parser setDelegate:self];
+    BOOL parsed = [parser parse];
+    if (!parsed)
+    {
+        // display a UIAlert indicating failure
+        [[[UIAlertView alloc] initWithTitle:@"Parse error"
+                                    message:@"We were unable to load xkcd; please check your Internet connection and try again later."
+                                   delegate:nil cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil]
+         show];
+    }
+}
+
+// MARK: -
+// MARK: NSXMLParserDelegate methods
+
+- (void)parser:(NSXMLParser *)parser
+didStartElement:(NSString *)elementName
+  namespaceURI:(NSString *)namespaceURI
+ qualifiedName:(NSString *)qualifiedName
+    attributes:(NSDictionary *)attributeDict
+{
+    if ([elementName isEqualToString:@"a"])
+    {
+        // 2. parse out the previous and next urls; store them
+        NSString *rel = [attributeDict valueForKey:@"rel"];
+        if ([rel isEqualToString:@"prev"])
+        {
+            previous = [attributeDict valueForKey:@"href"];
+        }
+        else if ([rel isEqualToString:@"next"])
+        {
+            next = [attributeDict valueForKey:@"href"];
+        }
+    }
+    else if (insideComicDiv && [elementName isEqualToString:@"img"])
+    {
+        // 3. parse out the URI for the comic's image; download it and render it in the comicView
+        NSString *src = [attributeDict valueForKey:@"src"];
+        NSURL *url = [NSURL URLWithString:src];
+        NSData *imageData = [NSData dataWithContentsOfURL:url];
+        [comicView setImage:[UIImage imageWithData:imageData]];
+
+        // 4. parse out the comic's alt-text and display it in altText
+        [altText setText:[attributeDict valueForKey:@"alt"]];
+
+        insideComicDiv = NO;
+    }
+    else if ([elementName isEqualToString:@"div"]
+             && [@"comic" isEqualToString:[attributeDict valueForKey:@"id"]])
+    {
+        insideComicDiv = YES;
+    }
 }
 
 @end
